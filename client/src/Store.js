0a3d4d5b8fcd7280d9eb8a029e92ceb7afa424ba
Ako Desmond <obendesmond2@gmail.com>
2019-11-13 09:44:13 +0100

    add redux-devtools-extension

--- client/src/Store.js
+++ client/src/Store.js
@@ -1,4 +1,4 @@
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import   rootReducer  from "./reducers";
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
