9b071dd61d1f840999d096f6a5ddbf5cbd54bb37
obendesmond <obendesmond2@gmail.com>
2019-11-15 13:52:11 +0100

    uncomment redux devtools

--- client/src/Store.js
+++ client/src/Store.js
@@ -6,16 +6,16 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const devtools =
+/*const devtools =
   process.env.NODE_ENV !== "production"
     ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__()
-    : () => {};
+    : () => {};*/
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(...middleware), devtools)
+  compose(applyMiddleware(...middleware), /*devtools*/)
 );
 
 export default store;
